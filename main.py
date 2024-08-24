6b7a65100bdbadb3db169bc47cbfa34bbe294926
56725676 <112773155+56725676@users.noreply.github.com>
2024-08-25 01:51:45 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+my_dict = {'denis': 7391, 'kate': 7392, 'dima': 7393}
+print(my_dict)
+print(my_dict ['dima'])
+print(my_dict.get('alex'))
+my_dict['egor'] = 7394
+my_dict['oleg'] = 7395
+del my_dict['dima']
+print(my_dict)
+
+my_set = {1, 2, 1, 2, 'a', 's', 'a', 's'}
+print(my_set)
+my_set.add(3)
+my_set.add('d')
+my_set.discard(1)
+print(my_set)
