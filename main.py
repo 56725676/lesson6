7756e4b8e1ad2f6f30c3beef259229b2a5dc450f
56725676 <112773155+56725676@users.noreply.github.com>
2024-08-25 19:33:58 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ print(my_dict ['dima'])
 print(my_dict.get('alex'))
 my_dict['egor'] = 7394
 my_dict['oleg'] = 7395
-del my_dict['dima']
-print(my_dict)
+a = my_dict.pop('dima')
+print(a)
 
 my_set = {1, 2, 1, 2, 'a', 's', 'a', 's'}
 print(my_set)
